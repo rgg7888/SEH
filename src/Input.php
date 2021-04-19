f04745b0be9d7b741e2351b5ee47963915f67e80
rgg7888 <rgg7888@outlook.com>
2021-04-19 15:32:32 -0600

    adding the input class fase 1

---
+++ src/Input.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App;
+
+class Input extends Base implements TagInterface {
+
+    public function whatItIs(char|string $char){
+        return match($char){
+            "C" => "class=\"",
+            "I" => "id=\"",
+            "T" => "type=\"",
+            "S" => "size=\"",
+            "O" => "onkeyup=\""
+        };
+    }
+
+    public function createOpenTag(array $attVal){
+        $string = implode(" ",$attVal);
+        return "<input ".$string."/>";
+    }
+
+    public function createCloseTag(array|string|null $content){
+        //none, Not Necesario para las selfclosing tags
+    }
+
+    public function tag(string|null $attrib=null, string|array|null $func=null){
+
+        $array=[];
+
+        if($this->args === null){
+            if($attrib !== null){
+                $array = explode(" ", $attrib);
+            }
+        }else{
+            $array = explode(" ", $this->getArgs());
+        }
+
+        $this->setArgs($this->createOpenTag($this->addAttrib($this->walkArray($array))));
+        return $this->getArgs();
+        /* Not Necesario para las selfclosing tags
+            if($func === null){
+                return $this->createCloseTag($this->getFunc());
+            }else{
+                return $this->createCloseTag($func);
+            }
+        */
+    }
+    /* Not Necesario para las selfclosing tags
+        public function setContent(string|array $content){
+            $this->setFunc($content);
+        }
+
+        public function getContent(){
+            $this->getFunc();
+        }
+    */
+    public function setAttributes(string|array $attributes){
+        $this->setArgs($attributes);
+    }
+
+    public function getAttributes(){
+        $this->getArgs();
+    }
+
+}
