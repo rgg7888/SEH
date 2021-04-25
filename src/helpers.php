2800b09682716c7fb610318f406c61bf3ac1b056
rgg7888 <rgg7888@outlook.com>
2021-04-25 17:02:58 -0600

    agregando la funcion my_site()

--- src/helpers.php
+++ src/helpers.php
@@ -301,3 +301,25 @@ if(!function_exists('pagina')) {
         echo App\ConstruirPieza::ensamblar( $piezasDeLaEtiqueta->unir( $piezas , $atributosDeLaEtiqueta->crearAtributos($atributos) ) );
     }
 }
+
+if(!function_exists('data_base_emulation')) {
+    function data_base_emulation() {
+        return [
+            'doctype' => ['<','!DOCTYPE',' html','>'],
+            'html' => ['<','html ','0','>','1','<','/','html','>']
+        ];
+    }
+}
+
+if(!function_exists('my_site')) {
+    function pagina($contenido = null,string $atributos = null) {
+        $etiqueta = new App\QuieroCrearUnaEtiqueta('doctype');
+        $piezas = $etiqueta->listaDinamicaDeEtiquetasYpiezas(data_base_emulation());
+        echo App\ConstruirPieza::ensamblar( $piezas );
+        $etiqueta->crearEtiqueta('html');
+        $piezas = $etiqueta->listaDinamicaDeEtiquetasYpiezas(data_base_emulation());
+        $atributosDeLaEtiqueta = new App\CrearAtributosDeLaEtiqueta();
+        $piezasDeLaEtiqueta = new App\AgregarLosAtributosALasPiezasYElContenido($contenido);
+        echo App\ConstruirPieza::ensamblar( $piezasDeLaEtiqueta->unir( $piezas , $atributosDeLaEtiqueta->crearAtributos($atributos) ) );
+    }
+}
