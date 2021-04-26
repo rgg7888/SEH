e1ac882a42c6c1579aeb1ee5242a2496c72783be
rgg7888 <rgg7888@outlook.com>
2021-04-25 19:19:38 -0600

    testing

--- src/CrearEventosClick.php
+++ src/CrearEventosClick.php
@@ -51,7 +51,8 @@ class CrearEventosClick {
     public function obtenerDatosGetSuperSimple(string $url, string $script) {
         $numFunciones = $this->obtenerEventos();
         #$peticion = "const xhr = new XMLHttpRequest();"."xhr.open(".htmlspecialchars("'GET','".$url."');")."xhr.send();";
-        $peticion = "xhr.open(".");";
+        #$peticion = "xhr.open(".");";
+        $peticion = "'GET','https://somenting.in'";
         $js = '';
         for($i = 0; $i < $numFunciones; $i++) {
             $js = str_replace($i,$peticion,$script);
