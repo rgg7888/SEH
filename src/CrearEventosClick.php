a841976ae7f65943f09b91fffe10b7257ceea9ec
rgg7888 <rgg7888@outlook.com>
2021-04-25 19:00:14 -0600

    testing

--- src/CrearEventosClick.php
+++ src/CrearEventosClick.php
@@ -50,7 +50,7 @@ class CrearEventosClick {
 
     public function obtenerDatosGetSuperSimple(string $url, string $script) {
         $numFunciones = $this->obtenerEventos();
-        $peticion = "const xhr = new XMLHttpRequest();"."xhr.open(".htmlspecialchars("'GET',".$url."');")."xhr.send();";
+        $peticion = "const xhr = new XMLHttpRequest();"."xhr.open(".htmlspecialchars("'GET','".$url."');")."xhr.send();";
         $js = '';
         for($i = 0; $i < $numFunciones; $i++) {
             $js = str_replace($i,$peticion,$script);
@@ -63,7 +63,7 @@ class CrearEventosClick {
         $numeroDeEventos = $this->obtenerEventos();
         for($i = 0; $i < $numeroDeEventos; $i++) {
             array_push($script, "Const ".$this->obtenerBotones()[$i]." = document.getElementById('".$this->obtenerIds()[$i]."');" );
-            array_push($script, "Const ".$this->obtenerFunciones()[$i]." = () => {".$i."};" );
+            array_push($script, "const ".$this->obtenerFunciones()[$i]." = () => {".$i."};" );
             array_push($script, $this->obtenerBotones()[$i].".addEventListener('click',".$this->obtenerFunciones()[$i].");" );
         }
         return implode("",$script);
