fb8116e73c6a7dc18b74ff1d36a64f864922a8e1
rgg7888 <rgg7888@outlook.com>
2021-04-28 16:21:11 -0600

    updating

--- src/helpers.php
+++ src/helpers.php
@@ -3,9 +3,19 @@
 //@@@###@@@### -> Utileria
 
 if(!function_exists('conectar')) {
-    function conectar($datosDeConexion) {
-        $conectar = new App\TransaccionesYconexionesConLaBaseDeDatos($datosDeConexion);
-        return $conectar->conectar();
+    function conectar() {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        try {
+        $conn = new PDO("mysql:host=$servername;dbname=seh", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        }
     }
 }
 
